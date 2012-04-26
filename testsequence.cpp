d690476e28a095bbe0ea64a530116def0e73c1a0
Abhinav Arora <abhinavdgr8bond@gmail.com>
2012-04-27 02:33:55 +0530

    TestSequence

---
+++ testsequence.cpp
@@ -0,0 +1,84 @@
+#include "testsequence.h"
+#include<iostream>
+using namespace std;
+
+TestSequence::TestSequence()
+{
+    //ctor
+    this->message = vector<Message>();
+    this->priority = 0;
+}
+
+TestSequence::TestSequence(TestSequence& another)
+{
+    //copy ctor
+    this->message = another.getTestSequence();
+    this->priority = another.getPriority();
+}
+
+void TestSequence::display()
+{
+    Message msg;
+    cout<<"Test Sequence : ";
+    for(unsigned int i =0; i<(this->message).size(); i++)
+    {
+        msg = (this->message)[i];
+        cout<<msg.getID()<<" ";
+    }
+    cout<<" | Priority: "<<(this->priority)<<endl;
+}
+
+int TestSequence::getLength()
+{
+    return (this->message).size();
+}
+
+int TestSequence::compareTo(TestSequence ts)
+{
+    int k = ts.getPriority() - (this->getPriority());
+    if(k==0)
+        return ((this->getLength() < ts.getLength()) ? (this->getLength()) : ts.getLength());
+    else
+        return k;
+}
+
+int TestSequence::getPriority()
+{
+    return (this->priority);
+}
+
+void TestSequence::setPriority(int priority)
+{
+    this->priority = priority;
+}
+
+void TestSequence::incrementPriority(int k)
+{
+    this->priority += k;
+}
+
+void TestSequence::decrementPriority(int k)
+{
+    this->priority -= k;
+}
+
+void TestSequence::addMessage(Message message)
+{
+    (this->message).push_back(message);
+}
+
+Message TestSequence::getLastMessage()
+{
+    return (this->message)[(this->message).size() -1];
+}
+
+vector<Message> TestSequence::getTestSequence()
+{
+    return this->message;
+}
+
+void TestSequence::appendSequence(TestSequence ts)
+{
+    vector<Message> temp = ts.getTestSequence();
+    (this->message).insert((this->message).end(), temp.begin(), temp.end());
+}
---
+++ testsequence.h
@@ -0,0 +1,37 @@
+#ifndef TESTSEQUENCE_H
+#define TESTSEQUENCE_H
+#define RETURN_MESSAGE_PRI 0
+#define MESSAGE_PRI 1
+#define LOOP_PRI 20
+#define ALT_PRI 9
+#define OPT_PRI 6
+#define BREAK_PRI 12
+#include "message.h"
+#include<vector>
+class TestSequence
+{
+    public:
+        /** Default constructor */
+        TestSequence();
+        /** Copy constructor
+         *  \param other Object to copy from
+         */
+        TestSequence(TestSequence&);
+        void display();
+        int getLength();
+        int compareTo(TestSequence);
+        int getPriority();
+        void setPriority(int);
+        void incrementPriority(int);
+        void decrementPriority(int);
+        void addMessage(Message);
+        Message getLastMessage();
+        vector<Message> getTestSequence();
+        void appendSequence(TestSequence);
+
+    private:
+        vector<Message> message;
+        int priority;
+};
+
+#endif // TESTSEQUENCE_H
